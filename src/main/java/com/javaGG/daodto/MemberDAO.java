3464f7d77c79728e17680f40e608cdfa12149560
GoRyeoIn <89904365+znzty15@users.noreply.github.com>
2021-09-29 10:19:52 +0900

    dao/dto EX

--- src/main/java/com/javaGG/daodto/MemberDAO.java
+++ src/main/java/com/javaGG/daodto/MemberDAO.java
@@ -24,25 +24,38 @@ public class MemberDAO {
 	public ArrayList<MemberDTO> memberSelect() {
 		ArrayList<MemberDTO> dtos = new ArrayList<MemberDTO>();
 		
-		Connection con;
-	    Statement stmt;
-	    ResultSet rs;
-	    con = DriverManager.getConnection(url, uid, upw);
-	    stmt = con.createStatement();
-	    rs = stmt.executeQuery(query);
+		Connection con = null;
+	    Statement stmt = null;
+	    ResultSet rs = null;
 	    
-	    while(rs.next()){
-	    	String name = rs.getString("name");
-            String id = rs.getString("id");
-            String pw = rs.getString("pw");
-            String ph1 = rs.getString("ph1");
-            String ph2 = rs.getString("ph2");
-            String ph3 = rs.getString("ph3");
-            String gender = rs.getString("gender");
-            
-            MemberDTO dto = new MemberDTO(name, id, pw, ph1, ph2, ph3, gender);
-            dtos.add(dto);
-         }
+	    try {
+	    	con = DriverManager.getConnection(url, uid, upw);
+		    stmt = con.createStatement();
+		    rs = stmt.executeQuery(query);		    
+		    while(rs.next()){
+		    	String name = rs.getString("name");
+	            String id = rs.getString("id");
+	            String pw = rs.getString("pw");
+	            String ph1 = rs.getString("ph1");
+	            String ph2 = rs.getString("ph2");
+	            String ph3 = rs.getString("ph3");
+	            String gender = rs.getString("gender");
+	            
+	            MemberDTO dto = new MemberDTO(name, id, pw, ph1, ph2, ph3, gender);
+	            dtos.add(dto);
+	         }
+	    }catch(SQLException e) {
+	    	e.printStackTrace();
+	    }finally {
+	    	try {
+				if(rs != null) rs.close();
+				if(stmt != null) stmt.close();
+				if(con != null) con.close();
+	    	}catch(SQLException e) {
+	    		e.printStackTrace();
+	    	}
+		}
+	    return dtos;
 	}
 	
 	
